15e2e34cf67ff862daa13273a1bec3faeb833340
NajaNaja <NajaNaja89@gmail.com>
2018-10-07 18:41:36 +0600

    first commit

---
+++ CSharpHomework3Module08/MyIndexer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework3Module08
+{
+    class MyIndexer
+    {
+        private double[] data;
+        private int size;
+        public MyIndexer(int size)
+        {
+            this.size = size;
+            data = new double[size];
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                return Math.Pow(data[index], 2);
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+
+    }
+}
---
+++ CSharpHomework3Module08/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework3Module08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            MyIndexer i1 = new MyIndexer(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine( $"Enter double value for {i} index");
+                string result = Console.ReadLine();
+                bool tryParse = double.TryParse(result, out double intResult);
+                try
+                {
+                    if (tryParse == false)
+                    {
+                        throw new Exception("Value must be double number!");
+                    }
+                    else i1[i] = intResult;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                    Environment.Exit(0);
+                }
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Result for {i} index is {i1[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
